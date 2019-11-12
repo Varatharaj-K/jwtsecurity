81db34c5d4c89a69cc0fef2bbbda099cdafdb9fe
Varatharaj <varatharajkvaj@gmail.com>
2019-11-12 18:13:30 +0530

    Initial Commit of JWT security

---
+++ src/main/java/com/jwttokensec/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.jwttokensec.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello";
+    }
+}
---
+++ src/main/java/com/jwttokensec/service/JwtUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.jwttokensec.service;
+
+import java.util.ArrayList;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        if ("varatharaj".equals(username)) {
+            return new User("varatharaj", "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6",
+                    new ArrayList<>());
+        } else {
+            throw new UsernameNotFoundException("User not found with username: " + username);
+        }
+    }
+}
+///home/tbi/jdk1.8.0_221
